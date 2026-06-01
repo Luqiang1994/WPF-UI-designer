b487d2c5154477dc95c6b3b24ac48fc889679274
Luqiang1994 <925722021@qq.com>
2026-06-01 22:40:15 +0800

    first commit

---
+++ Music Player/Music Player/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Music_Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
