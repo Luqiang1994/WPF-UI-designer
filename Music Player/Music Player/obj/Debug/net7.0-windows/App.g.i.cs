f7dbe873c13e58ec0aa5310904a23d010315c991
Luqiang1994 <925722021@qq.com>
2026-06-02 12:55:04 +0800

    完整版音乐播放器UI上传

--- Music Player/Music Player/obj/Debug/net7.0-windows/App.g.i.cs
+++ Music Player/Music Player/obj/Debug/net7.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace Music_Player {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace Music_Player {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Music Player;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Music Player;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace Music_Player {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
         public static void Main() {
             Music_Player.App app = new Music_Player.App();
             app.InitializeComponent();
